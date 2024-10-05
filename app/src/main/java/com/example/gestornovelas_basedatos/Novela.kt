02537d24d9fdbddde1a555b83108d87f3face22a
mbp4 <mblanpon@myuax.com>
2024-10-05 20:29:41 +0200

    creamos un constructor para poder inicializar las novelas

--- app/src/main/java/com/example/gestornovelas_basedatos/Novela.kt
+++ app/src/main/java/com/example/gestornovelas_basedatos/Novela.kt
@@ -1,3 +1,5 @@
 package com.example.gestornovelas_basedatos
 
-data class Novela(val titulo: String, val autor: String, val año: Int, val sinopsis: String)
+data class Novela(val titulo: String, val autor: String, val año: Int, val sinopsis: String){
+    constructor(): this("", "", 0, "")
+}
